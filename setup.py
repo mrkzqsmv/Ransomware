24c8907129c8ad96d39c3e728338a156cd25e9e3
mrkzqsmv <markazgasimov@gmail.com>
2024-12-22 18:56:39 +0400

    Added readme

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-# Setup script to build the executable
-setup(
-    name="MyApp",
-    version="1.0",
-    description="Python to ELF",
-    executables=[Executable("main_v2.py", target="my_app")]  # targetName -> target
-)
